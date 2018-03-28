63b6ec91a685702ee95d133717ccd7dbbc39bcb0
brianxautumn <shred@brianautumn.com>
2018-03-27 19:38:23 -0700

    Got rid of stray log

--- test/customFileExample.js
+++ test/customFileExample.js
@@ -1,15 +1,14 @@
-console.log('hello');
 const fileRequest = new XMLHttpRequest();
 fileRequest.open("GET", "PUT YOUR SAMPLE FILE HERE", true);
 fileRequest.responseType = "arraybuffer";
 
 const runTest = (buffer) =>{
-const demuxer = new OGVDemuxerWebM();
-    demuxer.receiveInput(buffer, () =>{
-    	demuxer.process((status) => {
-       		console.log(demuxer);
-       }); 
-    });
+	const demuxer = new OGVDemuxerWebM();
+	demuxer.receiveInput(buffer, () =>{
+		demuxer.process((status) => {
+			console.log(demuxer);
+		}); 
+	});
 };
 
 fileRequest.onload = (event) => {
