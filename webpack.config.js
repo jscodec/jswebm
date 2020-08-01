895f984f327ba47acb5c4bffe103df1983fc136e
brianxautumn <shred@brianautumn.com>
2020-07-31 21:28:03 -0700

    Bump version, fix output

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ module.exports = {
   entry: './src/JsWebm.js',
   mode: 'production',
   output: {
+    library: 'JsWebm',
+    libraryTarget: 'commonjs',
     path: path.resolve(__dirname, 'dist'),
     filename: 'JsWebm.js'
   }
